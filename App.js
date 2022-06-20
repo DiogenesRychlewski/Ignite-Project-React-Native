ad749072f72d03d45299b899004ace040ba5e7fb
Diógenes Rychlewski <dirychlewski@gmail.com>
2022-06-20 17:50:26 -0300

    Create Project p2

--- App.js
+++ App.js
@@ -71,8 +71,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            <Text style={styles.highlight}>MUDEI MEU APP</Text>
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
